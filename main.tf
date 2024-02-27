69513d1b8f983bd47a82189364f0d63722113070
gastonibarra <74162048+gastonibarra@users.noreply.github.com>
2024-02-26 23:50:06 -0300

    Added new files
    Added deploy for k8s and main.tf to deploy on eks.

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-west-2"  
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+resource "aws_subnet" "subnet_b" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"  
+}
+
+resource "aws_subnet" "subnet_c" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2c"  
+}
+
+module "eks_cluster" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.21"
+
+  subnet_ids              = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id, aws_subnet.subnet_c.id]
+  vpc_id                  = aws_vpc.my_vpc.id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+    }
+  }
+
+  tags = {
+    Environment = "test"
+  }
+}
